fedd4689d7e702b4f898ac8a064cfde7034c4550
mohamedaminearaied <mohamedamine.araied@isimg.tn>
2025-02-23 12:24:00 +0100

    Update Banque.java

--- src/b/Banque.java
+++ src/b/Banque.java
@@ -4,19 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 class Banque {
     private Map<Integer, Compte<?>> comptes = new HashMap<>();
-
     public void ajouterCompte(Compte<?> compte) {
         comptes.put(compte.numeroCompte, compte);
     }
-
     public Compte<?> rechercherCompte(int numeroCompte) {
         return comptes.get(numeroCompte);
     }
-
     public void supprimerCompte(int numeroCompte) {
         comptes.remove(numeroCompte);
     }
-
     public void afficherComptesTries() {
         comptes.values().stream()
         .sorted(Comparator.comparingDouble(compte -> ((Compte<Object>) compte).getSolde()).reversed()).forEach(compte -> {
@@ -24,6 +20,4 @@ class Banque {
                     compte.afficherSolde();
                 });
     }
-
-
 }
