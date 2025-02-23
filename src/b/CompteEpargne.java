c6e7158e62d2e6ebae5209f8a1d2a33208b26057
mohamedaminearaied <mohamedamine.araied@isimg.tn>
2025-02-23 12:25:15 +0100

    Update CompteEpargne.java

--- src/b/CompteEpargne.java
+++ src/b/CompteEpargne.java
@@ -1,12 +1,10 @@
 package b;
-
 class CompteEpargne<T> extends Compte<T> {
     private double taux;
     public CompteEpargne(int numeroCompte, double solde, T devise, double taux) {
         super(numeroCompte, solde, devise);
         this.taux = taux;
     }
-
     public void calculerInteret() {
         solde += solde * (taux/100);
     }
